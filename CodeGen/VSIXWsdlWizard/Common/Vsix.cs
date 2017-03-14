5b69866c79a92eecf09d59cb2232ad3fd006fcb6
zdyu <zdyu@Ctrip.com>
2017-03-14 13:02:11 +0800

    更改版本号为1.0.0

--- CodeGen/VSIXWsdlWizard/Common/Vsix.cs
+++ CodeGen/VSIXWsdlWizard/Common/Vsix.cs
@@ -11,7 +11,7 @@ namespace VSIXWsdlWizard.Common
         public const string Name = "Ant.SOA.CodeGen";
         public const string Description = "CodeGen For AntSoA frameworker Requires Visual Studio 2015";
         public const string Language = "en-US";
-        public const string Version = "1.0";
+        public const string Version = "1.0.0";
         public const string Author = "nainaigu";
         public const string Tags = "soa,contract";
     }
