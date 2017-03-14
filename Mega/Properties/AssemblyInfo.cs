5f8713e52cd0f6afb702474cfb691c94abc02a1c
zdyu <zdyu@Ctrip.com>
2017-03-14 12:58:57 +0800

    增加mega

---
+++ Mega/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AntServiceStack.Mega")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("nainaigu")]
+[assembly: AssemblyProduct("AntServiceStack.Mega")]
+[assembly: AssemblyCopyright("Copyright © Ant 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+//将 ComVisible 设置为 false 将使此程序集中的类型
+//对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("620a1d50-b1e0-4cdb-9dfd-4069939a54bd")]
+
