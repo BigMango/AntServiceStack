1a13ae1d9f97b9fc64ddb17b26027f9649e75ee7
1877682825@qq.com <1877682825@qq.com>
2017-03-27 09:05:56 +0800

    up

---
+++ Manager/AntServiceStack.Manager/Common/ConfigUtil.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudBag.Configuration
+{
+    public static class ConfigUtil
+    {
+        private static readonly AppSettingsReader configurationAppSettings;
+
+
+
+        static ConfigUtil()
+        {
+            configurationAppSettings = new System.Configuration.AppSettingsReader();
+        }
+
+        public static T GetConfig<T>(string key, T defaultValue)
+        {
+            try
+            {
+                var result = configurationAppSettings.GetValue(key, typeof(T));
+                return (T)result;
+            }
+            catch (Exception)
+            {
+                if (defaultValue != null)
+                {
+                    return defaultValue;
+                }
+                return default(T);
+            }
+        }
+
+        public static T GetConfig<T>(string key)
+        {
+            try
+            {
+                var result = configurationAppSettings.GetValue(key, typeof(T));
+                return (T)result;
+            }
+            catch (Exception)
+            {
+                throw new Exception(string.Format("没有在配置文件中的appSettings中找到{0}的配置，请检查配置文件配置！", key));
+            }
+        }
+
+        public static string GetConfig(string key)
+        {
+            try
+            {
+                var result = configurationAppSettings.GetValue(key, typeof(string));
+                return (string)result;
+            }
+            catch (Exception)
+            {
+                throw new Exception(string.Format("没有在配置文件中的appSettings中找到{0}的配置，请检查配置文件配置！", key));
+            }
+        }
+    }
+
+}
